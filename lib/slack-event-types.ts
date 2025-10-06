14b99be0c5dc338033e68fcff447ec3cfaa2019f
Hamad Riaz <hriaz@mobizinc.com>
2025-10-06 15:35:25 -0500

    Fixed imports

---
+++ lib/slack-event-types.ts
@@ -0,0 +1,34 @@
+export interface SlackBaseEvent {
+  type: string;
+  subtype?: string;
+  bot_id?: string;
+  bot_profile?: unknown;
+}
+
+export interface AppMentionEvent extends SlackBaseEvent {
+  type: "app_mention";
+  channel: string;
+  thread_ts?: string;
+  ts: string;
+  text: string;
+}
+
+export interface AssistantThreadStartedEvent extends SlackBaseEvent {
+  type: "assistant_thread_started";
+  assistant_thread: {
+    channel_id: string;
+    thread_ts: string;
+  };
+}
+
+export interface GenericMessageEvent extends SlackBaseEvent {
+  type: "message";
+  channel: string;
+  channel_type?: string;
+  thread_ts?: string;
+}
+
+export type SlackEvent =
+  | AppMentionEvent
+  | AssistantThreadStartedEvent
+  | GenericMessageEvent;
